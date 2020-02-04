5d6de5d91d389ba1a1e1f6c70eedc292b15609b3
hamada <hamashun18@yahoo.co.jp>
2020-02-04 18:02:10 +0900

    ユニコーンpidファイル保存先設定

--- db/schema.rb
+++ db/schema.rb
@@ -38,5 +38,17 @@ ActiveRecord::Schema.define(version: 2020_02_03_080409) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "products", "categories"
 end
