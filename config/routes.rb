bcd1ec3de6e6267255c87644e633fcceec9c87d3
hamada <hamashun18@yahoo.co.jp>
2020-02-05 17:24:33 +0900

    トップページ修正（ログイン状況によりヘッダーの表示変更）

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+
   
   root "products#index"
   get '/products/new', to: 'products#new'
+  get '/mypage', to: 'mypage#index'
 end
