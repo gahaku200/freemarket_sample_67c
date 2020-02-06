713998dd26f4c0f9162afa1c1dfe95f032a974f9
hamada <hamashun18@yahoo.co.jp>
2020-02-06 13:43:43 +0900

    form_withを使用してのデータの保存を確認（購入者のみnull）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,12 +9,15 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path, notice: "出品が完了しました"
+    end
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :status_id,  :delivery_charge_id, :ship_from_id, :delivery_days_id, :price).merge(seller_id: current_user.id)
+    params.require(:product).permit(:name, :description, :status_id,  :delivery_charge_id, :ship_from_id, :delivery_days_id, :price, :category_id).merge(seller_id: current_user.id)
   end
 
 
