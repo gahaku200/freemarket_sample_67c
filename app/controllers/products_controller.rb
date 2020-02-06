ee7d419327af7ed0e7ecec83ef8f48c3d00dd9d7
hamada <hamashun18@yahoo.co.jp>
2020-02-06 11:52:17 +0900

    テーブル間の条件付きアソシエーションの設定、インスタンスの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,19 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+
   end
+
+  def create
+    @product = Product.new(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :status_id,  :delivery_charge_id, :ship_from_id, :delivery_days_id, :price).merge(seller_id: current_user.id)
+  end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :delivery_days
+
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
 end
