76d9a8d381386b1b7d38b925cc035d5a6ff0be68
hamada <hamashun18@yahoo.co.jp>
2020-02-07 21:13:26 +0900

    モデル作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ApplicationRecord
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :delivery_days
+  belongs_to :category
+  belongs_to :brand, optional: true
+  accepts_nested_attributes_for :brand
 
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
