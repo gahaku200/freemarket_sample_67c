ac5f10dd763384473545c96a927ffecf121e0da6
hamada <hamashun18@yahoo.co.jp>
2020-02-14 20:58:32 +0900

    記述修正

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -3,7 +3,6 @@ class BuysController < ApplicationController
   before_action :set_card, :set_product
 
   def index
-    @user = User.find(current_user.id)
     @address = Address.find_by(user_id: current_user.id)
     @product_images = @product.images.limit(3)
     redirect_to root_path if @product.buyer != nil || @product.seller_id == current_user.id
@@ -20,9 +19,9 @@ class BuysController < ApplicationController
     else
       Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
       Payjp::Charge.create(
-        :amount => @product.price,          #支払金額を引っ張ってくる
-        :customer => @card.customer_id,  #顧客ID
-        :currency => 'jpy',              #日本円
+        amount: @product.price,          #支払金額を引っ張ってくる
+        customer: @card.customer_id,  #顧客ID
+        currency: 'jpy',              #日本円
       )
       if @product.update(buyer_id: current_user.id)
         redirect_to action: 'done'
