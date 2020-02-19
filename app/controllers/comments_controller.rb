417158799816c9546ac16a224f425b6ffd06858c
hamada <hamashun18@yahoo.co.jp>
2020-02-19 16:45:34 +0900

    コントローラー設定

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to controller: 'products', action: 'show'
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
+  end
+end
