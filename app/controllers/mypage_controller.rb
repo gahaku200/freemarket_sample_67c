8193e4e97f8d470c3f551cb07003e8e36cb9d615
hamada <hamashun18@yahoo.co.jp>
2020-02-20 13:41:11 +0900

    トップページにお知らせが届くように設定

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,12 +2,16 @@ class MypageController < ApplicationController
 
 
   def index
+    @products = Product.product
   end
 
   def card
   end
 
   def logout
+  end
+
+  def product_show
   end
 
   
